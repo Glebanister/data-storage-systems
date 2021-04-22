0967c4704dc9a0afc5698dbb1883a2b8f17868c4
Glebanister <glebmar2001@gmail.com>
2021-04-22 23:14:40 +0300

    Add lz4 as submodule

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <memory>
+
+#include "lz4.h"
+
+int main() {
+    char string[] = "Hello";
+    std::unique_ptr<char []> output = std::make_unique<char []>(10);
+    LZ4_compress_default(string, output.get(), sizeof(string), 10);
+}
